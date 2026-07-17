1a9681949bfbd7a75222f3f2bf3cea250233999b
ᵒʲⁱ <wargative@gmail.com>
2026-07-17 07:52:13 +0700

    Refactor BottomNav to use Link components

--- components/layout/BottomNav.tsx
+++ components/layout/BottomNav.tsx
@@ -1,18 +1,43 @@
+import Link from "next/link";
+
 const menus = [
-  "Dashboard",
-  "Markets",
-  "Trade",
-  "Points",
-  "Account",
+  {
+    name: "Dashboard",
+    href: "/",
+  },
+  {
+    name: "Markets",
+    href: "/markets",
+  },
+  {
+    name: "Trade",
+    href: "/trade",
+  },
+  {
+    name: "Points",
+    href: "/points",
+  },
+  {
+    name: "Account",
+    href: "/account",
+  },
 ];
 
 export default function BottomNav() {
   return (
     <nav className="fixed bottom-0 left-0 right-0 border-t border-[#252525] bg-[#151515]">
       <div className="flex justify-around py-3 text-xs text-[#A0A0A0]">
+
         {menus.map((menu) => (
-          <span key={menu}>{menu}</span>
+          <Link
+            key={menu.name}
+            href={menu.href}
+            className="transition hover:text-white"
+          >
+            {menu.name}
+          </Link>
         ))}
+
       </div>
     </nav>
   );
